ade4205df1758fd7ee42df1e595939eb60a792a8
BehnoodAmini <behnooda81@gmail.com>
2025-03-31 22:02:00 +0330

    middle banner update and delete

--- controllers/MiddleBannerCtrl.js
+++ controllers/MiddleBannerCtrl.js
@@ -2,8 +2,19 @@ const MiddleBanner = require("../models/MiddleBanner");
 
 const getAllMidBan = async (req, res) => {
   try {
-    const AllMidBans = await MiddleBanner.find();
-    res.status(200).json(AllMidBans);
+    if (req.query.pn) {
+      const paginate = 2;
+      const pageNumber = req.query.pn;
+      const GoalMidBans = await MiddleBanner.find()
+        .sort({ _id: -1 })
+        .skip((pageNumber - 1) * paginate)
+        .limit(paginate);
+      const AllMidBansNum = await (await MiddleBanner.find()).length;
+      res.status(200).json({ GoalMidBans, AllMidBansNum });
+    } else {
+      const AllMidBans = await MiddleBanner.find();
+      res.status(200).json(AllMidBans);
+    }
   } catch (err) {
     console.log(err);
     res.status(400).json({ msg: "error" });
@@ -13,7 +24,7 @@ module.exports.getAllMidBan = getAllMidBan;
 
 const newMidBan = async (req, res) => {
   try {
-    const newMidBan = new MiddleBanner({
+    const newMidBanner = new MiddleBanner({
       image: req.body.image,
       imageAlt: req.body.imageAlt,
       situation: req.body.situation,
@@ -23,7 +34,7 @@ const newMidBan = async (req, res) => {
         minute: "2-digit",
       }),
     });
-    newMidBan
+    newMidBanner
       .save()
       .then((d) => {
         res.status(200).json({ msg: "بنر میانی با موفقیت ذخیره شد." });
@@ -37,5 +48,51 @@ const newMidBan = async (req, res) => {
     res.status(400).json({ msg: "error" });
   }
 };
-
 module.exports.newMidBan = newMidBan;
+
+const updateMidBan = async (req, res) => {
+  try {
+    await MiddleBanner.updateOne(
+      { _id: req.body.goalId },
+      {
+        $set: {
+          image: req.body.image,
+          imageAlt: req.body.imageAlt,
+          situation: req.body.situation,
+          link: req.body.link,
+          date: new Date().toLocaleDateString("fa-IR", {
+            hour: "2-digit",
+            minute: "2-digit",
+          }),
+        },
+      }
+    );
+    res.status(200).json({ msg: "بنر میانی با موفقیت به روز رسانی شد." });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ msg: "error" });
+  }
+};
+module.exports.updateMidBan = updateMidBan;
+
+const deleteMidBan = async (req, res) => {
+  try {
+    await MiddleBanner.deleteOne({ _id: req.body.goalId });
+    res.status(200).json({ msg: "بنر میانی با موفقیت حذف شد." });
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ msg: "error" });
+  }
+};
+module.exports.deleteMidBan = deleteMidBan;
+
+const getOneMidBan = async (req, res) => {
+  try {
+    const goalMidBan = await MiddleBanner.findById(req.params.id);
+    res.status(200).json(goalMidBan);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ msg: "error" });
+  }
+};
+module.exports.getOneMidBan = getOneMidBan;
--- routes/MiddleBannerRoutes.js
+++ routes/MiddleBannerRoutes.js
@@ -5,5 +5,8 @@ const MiddleBannerCtrl = require("../controllers/MiddleBannerCtrl");
 
 router.get("/middle-banners", MiddleBannerCtrl.getAllMidBan);
 router.post("/new-middle-banner", MiddleBannerCtrl.newMidBan);
+router.post("/update-middle-banner", MiddleBannerCtrl.updateMidBan);
+router.post("/delete-middle-banner", MiddleBannerCtrl.deleteMidBan);
+router.get("/get-mid-ban/:id", MiddleBannerCtrl.getOneMidBan);
 
 module.exports = router;
